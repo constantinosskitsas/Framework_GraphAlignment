cab8b90e1fcdaf85c1a1ad9608d3c0bc333126b3
Cyfroni <karolplg@gmail.com>
2021-02-18 04:37:29 +0100

    reformat

--- workexp.py
+++ workexp.py
@@ -12,57 +12,40 @@ def global_config():
     gt = "data/example/gt.txt"
     data1 = "data/example/arenas_orig_100.txt"
     data2 = "data/example/arenas_orig_100.txt"
-    # data1 = "magkas.txt"
-    # data2 = "magkas1.txt"
-    G2 = ReadFile.edgelist_to_adjmatrix1(data2)
-    G1 = ReadFile.edgelist_to_adjmatrix1(data1)
-    G3 = ReadFile.edgelist_to_adjmatrix1(data2)
-    G3 = evaluation_design.remove_edges_directed(G3)
 
-    adj = ReadFile.edgelist_to_adjmatrixR(data1, data2)
-    adj1 = ReadFile.Edge_Removed_edgelist_to_adjmatrixR(G2, G3)
     gma, gmb = ReadFile.gt1(gt)
+    G1 = ReadFile.edgelist_to_adjmatrix1(data1)
+    G2 = ReadFile.edgelist_to_adjmatrix1(data2)
+    adj = ReadFile.edgelist_to_adjmatrixR(data1, data2)
 
 
 @ex.capture
-def eval_regal(_log, gma, gmb, adj, adj1):
+def eval_regal(_log, gma, gmb, adj):
     alignmatrix = regal.main(adj)
     mar, mbr = evaluation.transformRAtoNormalALign(alignmatrix)
-    acc1 = evaluation.accuracy(gma, gmb, mbr, mar)
-    _log.info(f"acc1: {acc1}")
+    acc = evaluation.accuracy(gma, gmb, mbr, mar)
+    _log.info(f"acc1: {acc}")
 
-    alignmatrix = regal.main(adj1)
-    mar, mbr = evaluation.transformRAtoNormalALign(alignmatrix)
-    acc = evaluation.check_with_identity(mbr+1)
-    _log.info(f"acc: {acc}")
-
-    return acc, acc1
+    return acc
 
 
 @ex.capture
-def eval_eigenalign(_log, gma, gmb, G1, G2, G3):
+def eval_eigenalign(_log, gma, gmb, G1, G2):
     ma1, mb1, _, _ = eigenalign.main(G1, G2, 8, "lowrank_svd_union", 3)
-    ma2, mb2, _, _ = eigenalign.main(G2, G3, 8, "lowrank_svd_union", 3)
-
     acc = evaluation.accuracy(gma+1, gmb+1, mb1, ma1)
     _log.info(f"acc: {acc}")
 
-    acc1 = evaluation.check_with_identity(mb2)
-    _log.info(f"acc1: {acc1}")
-
-    return acc, acc1
+    return acc
 
 
 @ex.capture
 def eval_conealign(_log, gma, gmb, G1, G2):
-
     alignmatrix = conealign.main(G1, G2)
     mar, mbr = evaluation.transformRAtoNormalALign(alignmatrix)
-    acc1 = evaluation.accuracy(gma, gmb, mbr, mar)
-    _log.info(f"acc1: {acc1}")
-    print(acc1)
-    print(mbr)
-    return acc1, acc1
+    acc = evaluation.accuracy(gma, gmb, mbr, mar)
+    _log.info(f"acc1: {acc}")
+
+    return acc
 
 
 @ex.capture
@@ -99,4 +82,3 @@ def main():
     eval_conealign()
     eval_netalign()
     eval_NSD()
-    print("MALAKA")

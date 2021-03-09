1d76b145b3bb05dcf30e6889ca7b137439ec5e67
Cyfroni <karolplg@gmail.com>
2021-03-09 23:25:21 +0100

    new eval, data-load

--- workexp.py
+++ workexp.py
@@ -1,7 +1,5 @@
 from algorithms import regal, eigenalign, conealign, netalign, NSD, klaus, gwl, isorank
 from data import similarities_preprocess
-# from data import ReadFile
-# from evaluation import evaluation, evaluation_design
 from sacred import Experiment
 import numpy as np
 import scipy.sparse as sps
@@ -37,17 +35,19 @@ def evall(gma, gmb, ma, mb):
 
 def e_to_G(e):
     n = np.amax(e) + 1
-    G = sps.csr_matrix((np.ones(e.shape[0]), e.T), shape=(n, n), dtype=int)
+    nedges = e.shape[0]
+    G = sps.csr_matrix((np.ones(nedges), e.T), shape=(n, n), dtype=int)
     G += G.T
     G.data = G.data.clip(0, 1)
-    return G.A
+    return G
 
 
-def e_to_Adj(e1, e2):
-    adj1 = sps.kron([[1, 0], [0, 0]], e_to_G(e1))
-    adj2 = sps.kron([[0, 0], [0, 1]], e_to_G(e2))
+def G_to_Adj(G1, G2):
+    adj1 = sps.kron([[1, 0], [0, 0]], G1)
+    adj2 = sps.kron([[0, 0], [0, 1]], G2)
     adj = adj1 + adj2
-    return adj.A
+    adj.data = adj.data.clip(0, 1)
+    return adj
 
 
 @ex.config
@@ -60,18 +60,9 @@ def global_config():
     target = "data/arenas_orig.txt"
     gt = f"data/noise_level_{noise_level}/gt_{edges}.txt"
 
-    # gma, gmb = ReadFile.gt1(gt)
-    # G1 = ReadFile.edgelist_to_adjmatrix1(data1)
-    # G2 = ReadFile.edgelist_to_adjmatrix1(data2)
-    # adj = ReadFile.edgelist_to_adjmatrixR(data1, data2)
-
     gma, gmb = np.loadtxt(gt, int).T
     Ae = np.loadtxt(data, int)
     Be = np.loadtxt(target, int)
-
-    G1 = e_to_G(Ae)
-    G2 = e_to_G(Be)
-    adj = e_to_Adj(Ae, Be)
 
 
 @ex.named_config
@@ -88,93 +79,67 @@ def demo():
     Ae = Ae[np.where(Ae < _lim, True, False).all(axis=1)]
     Be = gmb[Ae]
 
-    G1 = e_to_G(Ae)
-    G2 = e_to_G(Be)
-    adj = e_to_Adj(Ae, Be)
-
 
 @ex.capture
-def eval_regal(Ae, Be, gma, gmb, adj):
-    alignmatrix = regal.main(adj)
-    # ma, mb = evaluation.transformRAtoNormalALign(alignmatrix)
-    # acc = evaluation.accuracy(gma, gmb, mb, ma)
-    # print("acc:", acc)
+def eval_regal(Ae, Be, gma, gmb):
+    adj = G_to_Adj(e_to_G(Ae), e_to_G(Be))
 
+    alignmatrix = regal.main(adj.A)
     ma = np.arange(alignmatrix.shape[0])
     mb = alignmatrix.argmax(1).A1
+
     return evall(gma, gmb, ma, mb)
 
 
 @ex.capture
-def eval_eigenalign(Ae, Be, gma, gmb, G1, G2):
-    ma, mb, _, _ = eigenalign.main(G1, G2, 8, "lowrank_svd_union", 3)
-    # acc = evaluation.accuracy(gma+1, gmb+1, mb, ma)
-    # print("acc:", acc)
+def eval_eigenalign(Ae, Be, gma, gmb):
+    G1 = e_to_G(Ae)
+    G2 = e_to_G(Be)
+
+    ma, mb, _, _ = eigenalign.main(G1.A, G2.A, 8, "lowrank_svd_union", 3)
 
     return evall(gma, gmb, ma-1, mb-1)
 
 
 @ex.capture
-def eval_conealign(Ae, Be, gma, gmb, G1, G2):
-    alignmatrix = conealign.main(G1, G2)
-    # ma, mb = evaluation.transformRAtoNormalALign(alignmatrix)
-    # acc = evaluation.accuracy(gma, gmb, mb, ma)
-    # print("acc:", acc)
+def eval_conealign(Ae, Be, gma, gmb):
+    G1 = e_to_G(Ae)
+    G2 = e_to_G(Be)
 
+    alignmatrix = conealign.main(G1.A, G2.A)
     ma = np.arange(alignmatrix.shape[0])
     mb = alignmatrix.argmax(1).A1
+
     return evall(gma, gmb, ma, mb)
 
 
 @ex.capture
 def eval_netalign(Ae, Be, gma, gmb):
-    Ai, Aj = Ae.T
-    n = max(max(Ai), max(Aj)) + 1
-    nedges = len(Ai)
-    Aw = np.ones(nedges)
-    A = sps.csr_matrix((Aw, (Ai, Aj)), shape=(n, n), dtype=int)
-    A = A + A.T
-
-    Bi, Bj = Be.T
-    m = max(max(Bi), max(Bj)) + 1
-    medges = len(Bi)
-    Bw = np.ones(medges)
-    B = sps.csr_matrix((Bw, (Bi, Bj)), shape=(m, m), dtype=int)
-    B = B + B.T
-
+    A = e_to_G(Ae)
+    B = e_to_G(Be)
     L = similarities_preprocess.create_L(A, B, alpha=2)
     S = similarities_preprocess.create_S(A, B, L)
     li, lj, w = sps.find(L)
 
     ma, mb = netalign.main(S, w, li, lj, a=0)
+
     return evall(gma, gmb, ma, mb)
 
 
 @ex.capture
-def eval_NSD(Ae, Be, gma, gmb, G1, G2):
-    ma, mb = NSD.run(G2, G1)
-    # acc = evaluation.accuracy(gma, gmb, mb, ma)
-    # print(acc)
+def eval_NSD(Ae, Be, gma, gmb):
+    G1 = e_to_G(Ae)
+    G2 = e_to_G(Be)
+
+    ma, mb = NSD.run(G2.A, G1.A)
 
     return evall(gma, gmb, ma, mb)
 
 
 @ex.capture
 def eval_klaus(Ae, Be, gma, gmb):
-    Ai, Aj = Ae.T
-    n = max(max(Ai), max(Aj)) + 1
-    nedges = len(Ai)
-    Aw = np.ones(nedges)
-    A = sps.csr_matrix((Aw, (Ai, Aj)), shape=(n, n), dtype=int)
-    A = A + A.T
-
-    Bi, Bj = Be.T
-    m = max(max(Bi), max(Bj)) + 1
-    medges = len(Bi)
-    Bw = np.ones(medges)
-    B = sps.csr_matrix((Bw, (Bi, Bj)), shape=(m, m), dtype=int)
-    B = B + B.T
-
+    A = e_to_G(Ae)
+    B = e_to_G(Be)
     L = similarities_preprocess.create_L(A, B, alpha=4)
     S = similarities_preprocess.create_S(A, B, L)
     li, lj, w = sps.find(L)
@@ -209,25 +174,14 @@ def eval_gwl(Ae, Be, gma, gmb):
     }
 
     gwl.main(data)
+
     return (0, 0)
 
 
 @ex.capture
 def eval_isorank(Ae, Be, gma, gmb):
-    Ai, Aj = Ae.T
-    n = max(max(Ai), max(Aj)) + 1
-    nedges = len(Ai)
-    Aw = np.ones(nedges)
-    A = sps.csr_matrix((Aw, (Ai, Aj)), shape=(n, n), dtype=int)
-    A = A + A.T
-
-    Bi, Bj = Be.T
-    m = max(max(Bi), max(Bj)) + 1
-    medges = len(Bi)
-    Bw = np.ones(medges)
-    B = sps.csr_matrix((Bw, (Bi, Bj)), shape=(m, m), dtype=int)
-    B = B + B.T
-
+    A = e_to_G(Ae)
+    B = e_to_G(Be)
     L = similarities_preprocess.create_L(B, A, alpha=2)
     S = similarities_preprocess.create_S(B, A, L)
     li, lj, w = sps.find(L)
@@ -236,14 +190,11 @@ def eval_isorank(Ae, Be, gma, gmb):
     b = 0.8
     ma, mb = isorank.main(S, w, a, b, li, lj, 0)
 
-    # acc = evaluation.accuracy(gma+1, gmb+1, mb, ma)
-    # print(acc)
-
     return evall(gma, gmb, ma-1, mb-1)
 
 
 @ex.automain
-def main(Ae, Be, gma, gmb, G1, G2, adj):
+def main(Ae, Be, gma, gmb):
     np.set_printoptions(threshold=np.inf)
     print(np.array([gma, gmb]).T)
 

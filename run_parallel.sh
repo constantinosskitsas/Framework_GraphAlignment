87544569841297f865ed0e935e0b8302825e43e5
Cyfroni <karolplg@gmail.com>
2021-04-14 21:25:35 +0200

    freya adj.

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 graphs=$1
 cores=$2
@@ -25,7 +25,11 @@ step=$(((graphs+cores-1)/cores))
 for ((i=0;i<cores;i++)); do
     first=$((i*step+1))
     last=$(((i+1)*step))
-    python workexp.py with _giter=[$first,$last] seed=$seed $args &
+    python3 workexp.py with _giter=[$first,$last] seed=$((seed)) $args &
     echo $first $last
     sleep 0.1
 done
+
+# bash run_parallel.sh 15 3 "exp1 noise_level=0.05 full run=[2,6,7,8]" 005 seed=196355709
+# bash run_parallel.sh 15 3 "exp1 noise_level=0.04 full run=[2,6,7,8]" 004 seed=895321488
+# bash run_parallel.sh 15 3 "exp1 noise_level=0.03 full run=[2,6,7,8]" 003 seed=821564523

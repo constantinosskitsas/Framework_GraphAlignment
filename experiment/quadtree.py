55d4636d1e38cc0d9083439b7c229ae90254baa6
Cyfroni <karolplg@gmail.com>
2021-05-20 23:29:19 +0200

    pytorch only

--- experiment/quadtree.py
+++ experiment/quadtree.py
@@ -2,83 +2,61 @@ import numpy as np
 import torch
 
 
-def maxsquare(arr, x, y):
-    # return np.max(arr[x: x+1, y: y+1])
-    x1 = x+1
-    y1 = y+1
-    return max(
-        arr[x, y],
-        arr[x1, y],
-        arr[x, y1],
-        arr[x1, y1],
-    )
+# def maxsquare(arr, x, y):
+#     # return np.max(arr[x: x+1, y: y+1])
+#     x1 = x+1
+#     y1 = y+1
+#     return max(
+#         arr[x, y],
+#         arr[x1, y],
+#         arr[x, y1],
+#         arr[x1, y1],
+#     )
+#     # val1 = arr[x, y]
+#     # val2 = arr[x+1, y]
+#     # val3 = arr[x, y+1]
+#     # val4 = arr[x+1, y+1]
+#     # max1 = val1 if val1 > val2 else val2
+#     # max2 = val3 if val3 > val4 else val4
+#     # return max1 if max1 > max2 else max2
 
-    # val1 = arr[x, y]
-    # val2 = arr[x+1, y]
-    # val3 = arr[x, y+1]
-    # val4 = arr[x+1, y+1]
-    # max1 = val1 if val1 > val2 else val2
-    # max2 = val3 if val3 > val4 else val4
-    # return max1 if max1 > max2 else max2
+# _y = y << 1  # _y is divisible by 2
 
+# yy = torch.amax(prev_node[:, _y:_y + 2], dim=1)
+# current_node[:, y] = torch.amax(yy.view(-1, 2), dim=1)
 
-# def fun(i, a_name, a_shape, M_name, M_shape):
-#     a_m = shared_memory.SharedMemory(name=a_name)
-#     a = np.ndarray(a_shape, buffer=a_m.buf)
-#     M_m = shared_memory.SharedMemory(name=M_name)
-#     M = np.ndarray(M_shape, buffer=M_m.buf)
-#     _i = i << 1
-#     xx = np.amax(a[_i: _i + 2], axis=0)
-#     M[i] = np.amax(xx.reshape(-1, 2), axis=1)
+def quad_propagate(target, source, i, dim=0):
+    _i = i << 1
+    if dim == 0:
+        xx = torch.amax(source[_i: _i + 2], dim=dim)
+    else:
+        xx = torch.amax(source[:, _i: _i + 2], dim=dim)
+    if dim == 0:
+        target[i] = torch.amax(xx.view(-1, 2), dim=1)
+    else:
+        target[:, i] = torch.amax(xx.view(-1, 2), dim=1)
 
 
 def quad(array):
-    M = np.empty((array.shape[0]//2, array.shape[1]//2))
-
-    # sh_a = shared_memory.SharedMemory(create=True, size=array.nbytes)
-    # np.ndarray(array.shape, dtype=array.dtype, buffer=sh_a.buf)[:] = array
-
-    # sh_M = shared_memory.SharedMemory(create=True, size=M.nbytes)
-    # M = np.ndarray(M.shape, dtype=array.dtype, buffer=sh_M.buf)
-    # with Pool(5) as p:
-
-    #     args = [
-    #         (i, sh_a.name, array.shape, sh_M.name, M.shape) for i in range(M.shape[1])
-    #     ]
-    #     p.map(fun, args)
-    # print(M)
-    for i in range(M.shape[1]):
-        # for j in range(M.shape[0]):
-        #     M[j, i] = maxsquare(array, 2*j, 2*i)
-        _i = i << 1
-        xx = np.amax(array[_i: _i + 2], axis=0)
-        M[i] = np.amax(xx.reshape(-1, 2), axis=1)
-
-    return M  # , maxarg
-
-
-def quad_torch(array):
     M = torch.empty((array.shape[0]//2, array.shape[1]//2))
 
     for i in range(M.shape[1]):
 
-        _i = i << 1
-        xx = torch.amax(array[_i: _i + 2], dim=0)
-        M[i] = torch.amax(xx.reshape(-1, 2), dim=1)
+        quad_propagate(M, array, i)
+        # _i = i << 1
+        # xx = torch.amax(array[_i: _i + 2], dim=0)
+        # M[i] = torch.amax(xx.view(-1, 2), dim=1)
 
     return M
 
 
-def build_quadtree(matrix, _torch=False):
+def build_quadtree(matrix):
     quadtree = [matrix]
-    # indexes = []
     while quadtree[-1].shape != (1, 1):
-        # node, _max = quad(quadtree[-1])
-        node = quad_torch(quadtree[-1]) if _torch else quad(quadtree[-1])
+        node = quad(quadtree[-1])
         quadtree.append(node)
-        # indexes.append(_max)
-    # timings.append(time())
-    return quadtree  # , indexes
+
+    return quadtree
 
 
 def printquadtree(quadtree):
@@ -87,84 +65,83 @@ def printquadtree(quadtree):
             print(node)
 
 
-def reconstruct(quadtree, arg):
-    x, y = arg
-    quadtree[0][x] = -np.inf
-    quadtree[0][:, y] = -np.inf
-    # quadtree[0][x] = -1.0
-    # quadtree[0][:, y] = -1.0
-    for i in range(1, len(quadtree)):
-        # x //= 2
-        # y //= 2
-        current_node = quadtree[i]
-        prev_node = quadtree[i-1]
-        x >>= 1
-        y >>= 1
-        for j in range(current_node.shape[0]):
-            # quadtree[i][x, j] = maxsquare(quadtree[i-1], x*2, j*2)
-            # quadtree[i][j, y] = maxsquare(quadtree[i-1], j*2, y*2)
-            j2 = j << 1
-            current_node[x, j] = maxsquare(prev_node, x << 1, j2)
-            current_node[j, y] = maxsquare(prev_node, j2, y << 1)
+# def reconstruct(quadtree, arg):
+#     x, y = arg
+#     quadtree[0][x] = -np.inf
+#     quadtree[0][:, y] = -np.inf
+#     # quadtree[0][x] = -1.0
+#     # quadtree[0][:, y] = -1.0
+#     for i in range(1, len(quadtree)):
+#         # x //= 2
+#         # y //= 2
+#         current_node = quadtree[i]
+#         prev_node = quadtree[i-1]
+#         x >>= 1
+#         y >>= 1
+#         for j in range(current_node.shape[0]):
+#             # quadtree[i][x, j] = maxsquare(quadtree[i-1], x*2, j*2)
+#             # quadtree[i][j, y] = maxsquare(quadtree[i-1], j*2, y*2)
+#             j2 = j << 1
+#             current_node[x, j] = maxsquare(prev_node, x << 1, j2)
+#             current_node[j, y] = maxsquare(prev_node, j2, y << 1)
 
 
-def reconstruct_h(quadtree, arg):
-    x, y = arg
-    quadtree[0][x] = -np.inf
-    quadtree[0][:, y] = -np.inf
-    x_prop = range(quadtree[1].shape[0])
-    y_prop = range(quadtree[1].shape[0])
+# def reconstruct_h(quadtree, arg):
+#     x, y = arg
+#     quadtree[0][x] = -np.inf
+#     quadtree[0][:, y] = -np.inf
+#     x_prop = range(quadtree[1].shape[0])
+#     y_prop = range(quadtree[1].shape[0])
 
-    for i in range(1, len(quadtree)):
-        prev_node = quadtree[i-1]
-        current_node = quadtree[i]
-        # next_node = quadtree[i+1]
-        x >>= 1
-        y >>= 1
-        # print(x_prop)
-        # print(y_prop)
-        x_prop_new = set()
-        for x_p in x_prop:
-            val = current_node[x, x_p]
-            newval = maxsquare(prev_node, x << 1, x_p << 1)
-            if newval < val:
-                current_node[x, x_p] = newval
-                x_prop_new.add(x_p >> 1)
-        x_prop = x_prop_new
+#     for i in range(1, len(quadtree)):
+#         prev_node = quadtree[i-1]
+#         current_node = quadtree[i]
+#         # next_node = quadtree[i+1]
+#         x >>= 1
+#         y >>= 1
+#         # print(x_prop)
+#         # print(y_prop)
+#         x_prop_new = set()
+#         for x_p in x_prop:
+#             val = current_node[x, x_p]
+#             newval = maxsquare(prev_node, x << 1, x_p << 1)
+#             if newval < val:
+#                 current_node[x, x_p] = newval
+#                 x_prop_new.add(x_p >> 1)
+#         x_prop = x_prop_new
 
-        y_prop_new = set()
-        for y_p in y_prop:
-            val = current_node[y_p, y]
-            newval = maxsquare(prev_node, y_p << 1, y << 1)
-            if newval < val:
-                current_node[y_p, y] = newval
-                y_prop_new.add(y_p >> 1)
-        y_prop = y_prop_new
+#         y_prop_new = set()
+#         for y_p in y_prop:
+#             val = current_node[y_p, y]
+#             newval = maxsquare(prev_node, y_p << 1, y << 1)
+#             if newval < val:
+#                 current_node[y_p, y] = newval
+#                 y_prop_new.add(y_p >> 1)
+#         y_prop = y_prop_new
 
 
-def reconstruct_numpy(quadtree, arg):
-    x, y = arg
+# def reconstruct_numpy(quadtree, arg):
+#     x, y = arg
 
-    quadtree[0][x] = -np.inf
-    quadtree[0][:, y] = -np.inf
-    for i in range(1, len(quadtree)):
-        current_node = quadtree[i]
-        prev_node = quadtree[i-1]
+#     quadtree[0][x] = -np.inf
+#     quadtree[0][:, y] = -np.inf
+#     for i in range(1, len(quadtree)):
+#         current_node = quadtree[i]
+#         prev_node = quadtree[i-1]
 
-        x >>= 1
-        _x = x << 1  # _x is divisible by 2
-        xx = np.amax(prev_node[_x: _x + 2], axis=0)
-        current_node[x] = np.amax(xx.reshape(-1, 2), axis=1)
+#         x >>= 1
+#         _x = x << 1  # _x is divisible by 2
+#         xx = np.amax(prev_node[_x: _x + 2], axis=0)
+#         current_node[x] = np.amax(xx.reshape(-1, 2), axis=1)
 
-        y >>= 1
-        _y = y << 1  # _y is divisible by 2
-        yy = np.amax(prev_node[:, _y:_y + 2], axis=1)
-        current_node[:, y] = np.amax(yy.reshape(-1, 2), axis=1)
+#         y >>= 1
+#         _y = y << 1  # _y is divisible by 2
+#         yy = np.amax(prev_node[:, _y:_y + 2], axis=1)
+#         current_node[:, y] = np.amax(yy.reshape(-1, 2), axis=1)
 
 
 def reconstruct_pytorch(quadtree, arg):
     x, y = arg
-    # quadtree = [torch.from_numpy(arr) for arr in quadtree]
     quadtree[0][x] = float('-inf')
     quadtree[0][:, y] = float('-inf')
     for i in range(1, len(quadtree)):
@@ -172,30 +149,39 @@ def reconstruct_pytorch(quadtree, arg):
         prev_node = quadtree[i-1]
 
         x >>= 1
-        _x = x << 1  # _x is divisible by 2
-        xx = torch.amax(prev_node[_x: _x + 2], dim=0)
-        current_node[x] = torch.amax(xx.reshape(-1, 2), dim=1)
+
+        quad_propagate(current_node, prev_node, x)
+
+        # _x = x << 1  # _x is divisible by 2
+
+        # xx = torch.amax(prev_node[_x: _x + 2], dim=0)
+        # current_node[x] = torch.amax(xx.view(-1, 2), dim=1)
+
+        # xx = prev_node[_x: _x + 2].T.reshape(-1, 4)
+        # current_node[x] = torch.amax(xx, dim=1)
 
         y >>= 1
-        _y = y << 1  # _y is divisible by 2
-        yy = torch.amax(prev_node[:, _y:_y + 2], dim=1)
-        current_node[:, y] = torch.amax(yy.reshape(-1, 2), dim=1)
+
+        quad_propagate(current_node, prev_node, y, dim=1)
+
+        # _y = y << 1  # _y is divisible by 2
+
+        # yy = torch.amax(prev_node[:, _y:_y + 2], dim=1)
+        # current_node[:, y] = torch.amax(yy.view(-1, 2), dim=1)
+
+        # yy = prev_node[:, _y:_y + 2].reshape(-1, 4)
+        # current_node[:, y] = torch.amax(yy, dim=1)
 
 
-# reconstruct_numpy = reconstruct_pytorch
-
-
-def lookup(quadtree, _torch=False):
+def lookup(quadtree):
     x = y = 0
     for node in quadtree[::-1]:
         x *= 2
         y *= 2
         _x, _y = np.unravel_index(
-            torch.argmax(node[x:x+2, y:y+2]
-                         ) if _torch else np.argmax(node[x:x+2, y:y+2]),
+            torch.argmax(node[x:x+2, y:y+2]),
             shape=(2, 2)
         )
-        # print(_x, _y)
         x += _x
         y += _y
         # assert quadtree[-1][0, 0] == node[x, y]
@@ -212,49 +198,49 @@ def lookup(quadtree, _torch=False):
 #     return argmax, val
 
 
-def superfast_binbin(M):
+# def superfast_binbin(M):
 
+#     n = M.shape[0]
+
+#     quadtree = build_quadtree(M)
+
+#     # timings.append(time())
+
+#     ma = np.zeros(n, int)
+#     mb = np.zeros(n, int)
+#     # ma = torch.zeros(n, int)
+#     # mb = torch.zeros(n, int)
+
+#     for _ in range(n):
+#         # for _ in range(100):
+#         # argmax, maxval = quadtreepop(quadtree)
+
+#         # val = quadtree[-1][0, 0]
+#         argmax = lookup(quadtree)
+#         # timings.append(time())
+#         reconstruct_numpy(quadtree, argmax)
+#         # reconstruct_pytorch(quadtree, argmax)
+
+#         # timings.append(time())
+#         x, y = argmax
+#         ma[x] = x
+#         mb[x] = y
+#     # printquadtree(quadtree)
+#     return ma, mb
+
+
+def superfast_binbin_torch(M):
+    M = torch.from_numpy(M)
     n = M.shape[0]
 
     quadtree = build_quadtree(M)
 
-    # timings.append(time())
-
     ma = np.zeros(n, int)
     mb = np.zeros(n, int)
-    # ma = torch.zeros(n, int)
-    # mb = torch.zeros(n, int)
 
     for _ in range(n):
-        # for _ in range(100):
-        # argmax, maxval = quadtreepop(quadtree)
 
-        # val = quadtree[-1][0, 0]
         argmax = lookup(quadtree)
-        # timings.append(time())
-        reconstruct_numpy(quadtree, argmax)
-        # reconstruct_pytorch(quadtree, argmax)
-
-        # timings.append(time())
-        x, y = argmax
-        ma[x] = x
-        mb[x] = y
-    # printquadtree(quadtree)
-    return ma, mb
-
-
-def superfast_binbin_torch(M):
-
-    n = M.shape[0]
-
-    quadtree = build_quadtree(M, _torch=True)
-
-    ma = np.zeros(n, int)
-    mb = np.zeros(n, int)
-
-    for _ in range(n):
-
-        argmax = lookup(quadtree, _torch=True)
 
         reconstruct_pytorch(quadtree, argmax)
 
@@ -339,34 +325,54 @@ def superfast_binbin_torch(M):
 
 #     return ma, mb
 
+# def superfast(l2, asc=True):
+#     # print(f"superfast: init")
+#     # l2 = l2.A
+#     n = l2.shape[0]
+#     ma = np.zeros(n, int)
+#     mb = np.zeros(n, int)
+#     rows = set()
+#     cols = set()
+#     vals = np.argsort(l2, axis=None)
+#     vals = vals if asc else vals[::-1]
+
+#     # i = 0
+#     # for x, y in zip(*np.unravel_index(vals, l2.shape)):
+#     for val in vals:
+#         x, y = np.unravel_index(val, l2.shape)
+#         if x in rows or y in cols:
+#             continue
+#         # print(f"superfast: {i}/{n}")
+#         # i += 1
+#         ma[x] = x
+#         mb[x] = y
+
+#         rows.add(x)
+#         cols.add(y)
+#     return ma, mb
+
 
 if __name__ == "__main__":
 
     import sys
     import time
     k = int(sys.argv[1]) if len(sys.argv) > 1 else 4
-    test = len(sys.argv) > 2
 
     n = 2 ** k
-    # print(n)
-    # np.random.seed(1)
-    # print(np.random.random(5))
-    # exit()
-    # M = np.random.random(n*n).reshape(n, n).astype(dtype=np.float32)
+
     M = torch.rand(n, n, dtype=torch.float32)
-    # M = np.arange(n*n).reshape(n, n).astype(dtype=dtype)
-    # print(M)
 
     algs = [
         superfast_binbin_torch,
-        superfast_binbin,
-        # matching.superfast,
+        # superfast_binbin,
+        # superfast,
         # ultrafast,
     ]
 
     Ms = [
-        M.clone(),
-        M.clone().numpy(),
+        M.numpy(),
+        # M.clone().numpy(),
+        # -M.clone().numpy(),
         # M.clone().numpy(),
     ]
     args = []
@@ -379,10 +385,8 @@ if __name__ == "__main__":
 
         print([timings[i] - timings[i-1] for i in range(1, len(timings))])
 
-    if not test:
+    if len(args) > 1:
         assert np.all(args[0][1] == args[1][1])
-    # print(args1)
-    # print(args2)
 
     print("n:", n)
 

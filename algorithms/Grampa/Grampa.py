5cdda59009ff579fde99ad7b066a45e1e638f804
constantinosskitsas <constantinosskitsas@hotmail.com>
2022-05-23 12:42:52 +0200

    Grampa

--- algorithms/Grampa/Grampa.py
+++ algorithms/Grampa/Grampa.py
@@ -13,11 +13,36 @@ def seigh(A):
   Sort eigenvalues and eigenvectors in descending order. 
   Not used.
   """
-    l, u = np.linalg.eigh(A)
-    idx = l.argsort()[::-1]   
-    l = l[idx]
-    u = u[:,idx]
-    return l, u
+  l, u = np.linalg.eigh(A)
+  idx = l.argsort()[::-1]   
+  l = l[idx]
+  u = u[:,idx]
+  return l, u
+def main(data, eta):
+  Src = data['Src']
+  Tar = data['Tar']
+  n = Src.shape[0]
+  l,U =eigh(Src)
+  mu,V = eigh(Tar)
+  l = np.array([l])
+  mu = np.array([mu])
+
+  #Eq.4
+  coeff = 1.0/((l.T - mu)**2 + eta**2)
+  #Eq. 3
+  coeff = coeff * (U.T @ np.ones((n,n)) @ V)
+  X = U @ coeff @ V.T
+
+  Xt = X.T
+  Xt=X
+  # Solve with linear assignment maximizing the similarity 
+  # row,col = linear_sum_assignment(Xt, maximize=True)
+
+  # Alternatively, we can use a more efficient solver.
+  # The solver works on cost minimization, so take -X 
+  #rows, cols = solve_dense(-Xt)
+  #return rows, cols 
+  return Xt
 
 def grampa(Src, Tar, eta):
   """
@@ -31,19 +56,19 @@ def grampa(Src, Tar, eta):
   Returns:
   Xt similarity Matrix
   """
-    n = Src.shape[0]
-    l,U = eigh(Src)
-    mu,V = eigh(Tar)
-    l = np.array([l])
-    mu = np.array([mu])
+  n = Src.shape[0]
+  l,U = eigh(Src)
+  mu,V = eigh(Tar)
+  l = np.array([l])
+  mu = np.array([mu])
 
   #Eq.4
-    coeff = 1.0/((l.T - mu)**2 + eta**2)
+  coeff = 1.0/((l.T - mu)**2 + eta**2)
   #Eq. 3
-    coeff = coeff * (U.T @ np.ones((n,n)) @ V)
-    X = U @ coeff @ V.T
+  coeff = coeff * (U.T @ np.ones((n,n)) @ V)
+  X = U @ coeff @ V.T
 
-    Xt = X.T
+  Xt = X.T
   # Solve with linear assignment maximizing the similarity 
   # row,col = linear_sum_assignment(Xt, maximize=True)
 
@@ -51,4 +76,4 @@ def grampa(Src, Tar, eta):
   # The solver works on cost minimization, so take -X 
   #rows, cols = solve_dense(-Xt)
   #return rows, cols 
-    return Xt
+  return Xt
